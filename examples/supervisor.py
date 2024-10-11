c58270ad58f969e993a1b0a16a173761487259a5
budzianowski <budzianowski@gmail.com>
2024-10-10 17:43:56 -0700

    add example

--- examples/supervisor.py
+++ examples/supervisor.py
@@ -4,27 +4,27 @@ import argparse
 import math
 import time
 
-from actuator import RobstrideMotorsSupervisor
+from actuator import RobstrideMotors
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port-name", type=str, default="/dev/ttyUSB0")
-    parser.add_argument("--motor-id", type=int, default=1)
-    parser.add_argument("--motor-type", type=str, default="01")
+    parser.add_argument("--port-name", type=str, default="/dev/tty.usbserial-210")
+    parser.add_argument("--motor-id", type=int, default=127)
+    parser.add_argument("--motor-type", type=str, default="04")
     args = parser.parse_args()
 
-    supervisor = RobstrideMotorsSupervisor(args.port_name, {args.motor_id: args.motor_type})
-    supervisor.add_motor_to_zero(args.motor_id)
+    supervisor = RobstrideMotors(args.port_name, {args.motor_id: args.motor_type})
+    supervisor.send_get_mode()
     start_time = time.time()
 
     try:
         while True:
             time.sleep(0.25)
             target_position = 0.5 * math.sin(time.time() - start_time)
-            supervisor.set_target_position(args.motor_id, target_position)
-            # feedback = supervisor.get_latest_feedback()
-            # print(feedback)
+            # supervisor.set_target_position(args.motor_id, target_position)
+            feedback = supervisor.get_latest_feedback()
+            print(feedback)
 
     except KeyboardInterrupt:
         supervisor.stop()
