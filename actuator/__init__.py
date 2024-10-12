e5bd639866f31833a3307ca8713b14d3634c7b78
Benjamin Bolte <ben@bolte.cc>
2024-10-12 12:22:04 -0700

    updated api

--- actuator/__init__.py
+++ actuator/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.0.14"
 
 from .rust.bindings import (
+    PyRobstrideMotorControlParams as RobstrideMotorControlParams,
     PyRobstrideMotorFeedback as RobstrideMotorFeedback,
     PyRobstrideMotors as RobstrideMotors,
     PyRobstrideMotorsSupervisor as RobstrideMotorsSupervisor,
