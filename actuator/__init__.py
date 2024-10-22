3a98dce38cbb8f19e282392c86fc2bbe154630cc
WT-MM <wesley.maa@gmail.com>
2024-10-22 13:28:55 -0700

    Bump version to 0.0.23

--- actuator/__init__.py
+++ actuator/__init__.py
@@ -1,6 +1,6 @@
 """Defines the top-level API for the actuator package."""
 
-__version__ = "0.0.22"
+__version__ = "0.0.23"
 
 from .rust.bindings import (
     PyRobstrideMotorConfig as RobstrideMotorConfig,
