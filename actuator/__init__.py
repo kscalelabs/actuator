9a6a037f28a03e4fc186b43d58c50652434bce60
Benjamin Bolte <ben@bolte.cc>
2024-10-24 16:23:29 -0700

    un-bump version

--- actuator/__init__.py
+++ actuator/__init__.py
@@ -1,6 +1,6 @@
 """Defines the top-level API for the actuator package."""
 
-__version__ = "0.0.25"
+__version__ = "0.0.24"
 
 from .bindings import (
     PyRobstrideMotorConfig as RobstrideMotorConfig,
